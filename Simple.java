f13970657717801b9a03792cc524afc1cda42dfd
Sunidhi Singh <75873828+Sunport15@users.noreply.github.com>
2024-09-09 01:38:08 +0000

    Add files via upload

---
+++ Simple.java
@@ -0,0 +1,10 @@
+public class Simple {  
+    public static void main(String args[]) {  
+        printMessage("Hello Java");
+        printMessage("Welcome to Java Programming!");
+    }  
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+}
